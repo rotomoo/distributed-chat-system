a740f2cd4d277b39909f1422b33fa0616e9b2017
tomoo <quddlf410@naver.com>
2024-12-03 10:47:38 +0900

    feat: 웹소켓 세션 정보 추가

--- module/project/distributed-chat-system-chatting/src/main/java/com/distributed/chat/system/chatting/base/handler/CustomTextWebSocketHandler.java
+++ module/project/distributed-chat-system-chatting/src/main/java/com/distributed/chat/system/chatting/base/handler/CustomTextWebSocketHandler.java
@@ -1,13 +1,17 @@
 package com.distributed.chat.system.chatting.base.handler;
 
+import com.distributed.chat.system.chatting.web.dto.ChatMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -26,11 +30,25 @@ public class CustomTextWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        Principal principal = session.getPrincipal();
+        String sender = (principal != null) ? principal.getName() : "Anonymous";
+
         log.info("{} sent {}", session.getId(), message.getPayload());
+        
+        ChatMessage chatMessage = new ChatMessage(session.getId(), message.getPayload().toString());
+
+        // {"sender":"fee1fa63-a0c2-d8e0-234a-510575341f9c","message":"124v14"}
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonMessage = objectMapper.writeValueAsString(chatMessage);
+        WebSocketMessage<String> newMessage = new TextMessage(jsonMessage);
+
+        // fee1fa63-a0c2-d8e0-234a-510575341f9c:124
+//        String plainMessage = session.getId() + ":" + message.getPayload();
+//        WebSocketMessage<String> newMessage = new TextMessage(plainMessage);
 
         this.webSocketSessionMap.values().forEach(webSocketSession -> {
             try {
-                webSocketSession.sendMessage(message);
+                webSocketSession.sendMessage(newMessage);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
