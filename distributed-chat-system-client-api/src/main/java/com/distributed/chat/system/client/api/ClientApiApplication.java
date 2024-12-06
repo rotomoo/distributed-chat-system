23c42a6f1a1007cbd2b4e9df4898755f805f4068
tomoo <quddlf410@naver.com>
2024-12-06 14:14:26 +0900

    feat: client-api 모듈 service-discovery feign client 추가

--- distributed-chat-system-client-api/src/main/java/com/distributed/chat/system/client/api/ClientApiApplication.java
+++ distributed-chat-system-client-api/src/main/java/com/distributed/chat/system/client/api/ClientApiApplication.java
@@ -2,8 +2,10 @@ package com.distributed.chat.system.client.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.distributed")
+@EnableFeignClients
 public class ClientApiApplication {
 
     public static void main(String[] args) {
