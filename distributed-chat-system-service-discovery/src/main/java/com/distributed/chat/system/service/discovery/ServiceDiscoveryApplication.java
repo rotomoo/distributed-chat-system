0c0c082eb2ef7ff44968cc3e6a5f22db8449a47f
tomoo <quddlf410@naver.com>
2024-12-11 15:59:28 +0900

    feat: client-side discovery 패턴 적용, chatting service loadbalancer 적용

--- distributed-chat-system-service-discovery/src/main/java/com/distributed/chat/system/service/discovery/ServiceDiscoveryApplication.java
+++ distributed-chat-system-service-discovery/src/main/java/com/distributed/chat/system/service/discovery/ServiceDiscoveryApplication.java
@@ -2,10 +2,10 @@ package com.distributed.chat.system.service.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.distributed")
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
